0e16b74841496923d268ab2a35f1692a15be3a20
lilly <lilly1987@naver.com>
2022-03-12 16:36:04 +0900

    ...

--- COM3D2.PartsEdit.Plugin/MaidObserver.cs
+++ COM3D2.PartsEdit.Plugin/MaidObserver.cs
@@ -4,39 +4,51 @@ using System.Linq;
 using System.Text;
 
 using UnityEngine;
+namespace CM3D2.PartsEdit.Plugin
+{
+    class MaidObserver
+    {
+        HashSet<Maid> maidHash = null;
 
-class MaidObserver {
-    HashSet<Maid> maidHash = null;
-
-    public bool CheckActiveChange() {
-        if (maidHash == null) return true;
-        List<Maid> activeMaidList = GetActiveMaidList();
-        if (maidHash.Count != activeMaidList.Count) {
-            return true;
-        }
-        foreach (Maid maid in activeMaidList) {
-            if (!maidHash.Contains(maid)) {
+        public bool CheckActiveChange()
+        {
+            if (maidHash == null) return true;
+            List<Maid> activeMaidList = GetActiveMaidList();
+            if (maidHash.Count != activeMaidList.Count)
+            {
                 return true;
             }
-        }
-        return false;
-    }
-
-    public void UpdateMaidActiveState() {
-        maidHash = new HashSet<Maid>(GetActiveMaidList());
-    }
-
-    public List<Maid> GetActiveMaidList() {
-        List<Maid> activeMaidList = new List<Maid>();
-        for(int i = 0; i < GameMain.Instance.CharacterMgr.GetMaidCount(); i++) {
-            Maid maid = GameMain.Instance.CharacterMgr.GetMaid(i);
-            if(maid==null || !maid.gameObject.activeInHierarchy) {
-                continue;
+            foreach (Maid maid in activeMaidList)
+            {
+                if (!maidHash.Contains(maid))
+                {
+                    return true;
+                }
             }
-            activeMaidList.Add(maid);
+            return false;
         }
-        List<Maid> stockList = GameMain.Instance.CharacterMgr.GetStockMaidList();
-        activeMaidList.AddRange(stockList.Where(m => (m && m.gameObject.activeInHierarchy)));
-        return activeMaidList.Distinct().ToList();
+
+        public void UpdateMaidActiveState()
+        {
+            maidHash = new HashSet<Maid>(GetActiveMaidList());
+        }
+
+        public List<Maid> GetActiveMaidList()
+        {
+            List<Maid> activeMaidList = new List<Maid>();
+            for (int i = 0; i < GameMain.Instance.CharacterMgr.GetMaidCount(); i++)
+            {
+                Maid maid = GameMain.Instance.CharacterMgr.GetMaid(i);
+                if (maid == null || !maid.gameObject.activeInHierarchy)
+                {
+                    continue;
+                }
+                activeMaidList.Add(maid);
+            }
+            List<Maid> stockList = GameMain.Instance.CharacterMgr.GetStockMaidList();
+            activeMaidList.AddRange(stockList.Where(m => (m && m.gameObject.activeInHierarchy)));
+            return activeMaidList.Distinct().ToList();
+        }
     }
+
 }
